64fcbf4b983189d33eaac49ba109331e5d536c50
anand-184 <anandita9464@gmail.com>
2025-06-29 16:02:03 +0530

    commit-Text&Modifiers

--- app/src/main/java/com/anand/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/anand/jetpackcompose/MainActivity.kt
@@ -1,23 +1,31 @@
 package com.anand.jetpackcompose
-
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.anand.jetpackcompose.ui.theme.JetPackComposeTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,33 +33,60 @@ class MainActivity : ComponentActivity() {
         installSplashScreen()
         enableEdgeToEdge()
         setContent {
-            JetPackComposeTheme {
+            JetPackComposeTheme{
                 Box(
-                    modifier =Modifier.fillMaxSize(),
-                    contentAlignment = Alignment.Center){
-                    Greeting("Kotlin X JetpackCompose")
-
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
+                    contentAlignment = Alignment.Center // centers the Column
+                ) {
+                    TitleWithAuthor()
+                }
                 }
             }
         }
     }
-}
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "$name!",
-        modifier = modifier,
-        color = Color(0xFF6650a4),
-        fontSize = 24.sp,
-        fontFamily = FontFamily.Serif
-    )
-}
+fun TitleWithAuthor(){
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Text(text="Kotlin X Jetpack Compose",
+            color =Color(0xFF6C4A8C),
+            fontFamily = FontFamily.SansSerif,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+        Row(  modifier = Modifier.align(Alignment.End)
+            .padding(top = 4.dp),
+            horizontalArrangement = Arrangement.End) {
+            val context = LocalContext.current
+            Text(
+                text = "-By Anandita",
+                modifier = Modifier
+                    .background(Color(0xFFD0BCFF))
+                    .clickable {
+                        Toast.makeText(context, "Developed By: Er.Anandita", Toast.LENGTH_SHORT).show()
+                    }
+                    .padding(6.dp),
+                color = Color(0xFF77194A),
+                fontFamily = FontFamily.Cursive,
+                fontStyle = androidx.compose.ui.text.font.FontStyle.Italic
+            )
+        }
+    }
 
+
+}
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JetPackComposeTheme {
-        Greeting("Kotlin X JetpackCompose")
+        TitleWithAuthor()
     }
 }
+
+
+
