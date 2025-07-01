b14e7a031e511840ae4b2b0d332c521862443f75
anand-184 <anandita9464@gmail.com>
2025-07-01 15:43:22 +0530

    commit-Text&Modifiers,Box,rows,columns

--- app/src/main/java/com/anand/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/anand/jetpackcompose/MainActivity.kt
@@ -11,9 +11,15 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -34,57 +40,60 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JetPackComposeTheme{
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(16.dp),
-                    contentAlignment = Alignment.Center // centers the Column
-                ) {
-                    TitleWithAuthor()
-                }
+                ProfileCard()
+
                 }
             }
         }
     }
 
 @Composable
-fun TitleWithAuthor(){
-    Column(
-        modifier = Modifier.padding(16.dp)
-    ) {
-        Text(text="Kotlin X Jetpack Compose",
-            color =Color(0xFF6C4A8C),
-            fontFamily = FontFamily.SansSerif,
-            fontSize = 24.sp,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(bottom = 8.dp)
-        )
-        Row(  modifier = Modifier.align(Alignment.End)
-            .padding(top = 4.dp),
-            horizontalArrangement = Arrangement.End) {
+fun ProfileCard(){
+    var outerBg by remember { mutableStateOf(Color.DarkGray) }
+    Box(Modifier.fillMaxSize().padding(16.dp).background(outerBg)){
+        Box(Modifier.fillMaxSize().padding(top = 50.dp, start = 20.dp, end = 20.dp, bottom = 50.dp).align(Alignment.Center).background(Color.LightGray)){
             val context = LocalContext.current
-            Text(
-                text = "-By Anandita",
-                modifier = Modifier
-                    .background(Color(0xFFD0BCFF))
-                    .clickable {
-                        Toast.makeText(context, "Developed By: Er.Anandita", Toast.LENGTH_SHORT).show()
-                    }
-                    .padding(6.dp),
-                color = Color(0xFF77194A),
-                fontFamily = FontFamily.Cursive,
-                fontStyle = androidx.compose.ui.text.font.FontStyle.Italic
-            )
+            val onClick={Toast.makeText(context,"Welcome to JetPack Compose",Toast.LENGTH_SHORT).show()
+                outerBg = Color.White}
+            Column(modifier = Modifier.align(Alignment.Center).padding(top = 15.dp).background(Color.White).
+            clickable(onClick = onClick)){
+                Text(text = "Er.Anandita",fontSize = 30.sp,color = Color(0xFF452073),
+                    fontWeight = FontWeight.Bold,
+                    fontFamily = FontFamily.Cursive,
+                    modifier = Modifier.padding(16.dp).align(Alignment.CenterHorizontally))
+                Text(text = "App Developer",
+                    fontSize = 18.sp,
+                    fontFamily = FontFamily.Monospace,
+                    color = Color(0xFF452073),
+                    modifier = Modifier.padding(start = 16.dp).align(Alignment.CenterHorizontally))
+
+                Row(modifier = Modifier.padding(16.dp).align(Alignment.CenterHorizontally), horizontalArrangement = Arrangement.Center){
+                    Text(text = "Kotlin | ",
+                        color = Color.Black,
+                        fontSize = 16.sp)
+                    Text(text="Python | ",fontSize = 16.sp,
+                        color = Color.Black)
+                    Text(text = "JetPack Compose",fontSize = 16.sp,
+                        color = Color.Black, modifier = Modifier.align(Alignment.CenterVertically))
+                }
+            }
+            Column(modifier = Modifier.align(Alignment.BottomEnd).padding(bottom = 20.dp, end = 10.dp)){
+                Text(text = "📞: +91 XXXXXXXXX",fontSize = 18.sp,color=Color.Black,
+                    modifier = Modifier.padding(start = 16.dp).align(Alignment.CenterHorizontally))
+
+            }
         }
     }
-
-
 }
+
+
+
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JetPackComposeTheme {
-        TitleWithAuthor()
+        ProfileCard()
     }
 }
 
